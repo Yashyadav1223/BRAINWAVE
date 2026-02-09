feef1fca2b5074c8754b1f6732cc1c97a3f48053
Yash Kumar <yashyadav244221@gmail.com>
2026-02-09 23:59:33 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import ButtonGradient from "./assets/svg/ButtonGradient"
+import Button from "./components/button";
+import Header from "./components/Header";
+import Hero from "./components/Hero";
+import Benefits from "./components/Benefits";
+function App() {
+  return (
+    <>
+    
+    <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden">
+      <Header />
+      <Hero />
+      <Benefits/>
+     
+    </div>
+    <ButtonGradient/>
+    </>
+  )
+}
+
+export default App;
---
+++ src/components/Benefits.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Section from './Section'
+import Heading from './Heading'
+
+const Benefits = () => {
+  return (
+    <Section id="features"> 
+      <div className='container relative z-2'>
+        <Heading/>
+      </div>
+
+    </Section>
+  )
+}
+
+export default Benefits
