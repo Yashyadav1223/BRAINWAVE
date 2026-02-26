9dca0318748fece83eaa0ddd321df9111f5da5d5
Yash Kumar <yashyadav244221@gmail.com>
2026-02-26 22:45:19 +0530

    Fixed case-sensitive import issue

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import ButtonGradient from "./assets/svg/ButtonGradient"
-import Button from "./components/Button";
+
 import Header from "./components/Header";
 import Hero from "./components/Hero";
 import Benefits from "./components/Benefits";
